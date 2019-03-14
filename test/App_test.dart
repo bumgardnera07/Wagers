cfa5326073cb06cc4226e1025c05ac49d5941701
Unknown <mahuynh@microsoft.com>
2019-03-13 23:48:02 -0700

    widget test: can navigate to bets screen from home screen

--- test/App_test.dart
+++ test/App_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shot_bets/BetsListGroupView.dart';
 import 'package:shot_bets/CreateBetIconButton.dart';
 import 'package:shot_bets/main.dart';
 
@@ -8,7 +9,6 @@ void main() {
   // for us to work with. The WidgetTester will allow us to build and interact
   // with Widgets in the test environment.
   testWidgets('Home screen has proper layout and look', (WidgetTester tester) async {
-
     await tester.pumpWidget(
         MyApp()
     );
@@ -46,5 +46,18 @@ void main() {
         find.descendant(of: navBarFinder, matching: find.text("Settings")),
         findsOneWidget
     );
+  });
+
+  testWidgets('Can navigate to bets screen from home screen', (WidgetTester tester) async {
+    await tester.pumpWidget(
+        MyApp()
+    );
+
+    // press the Bets icon button
+    await tester.tap(find.widgetWithText(BottomNavigationBar, "Bets"));
+    await tester.pumpAndSettle();
+
+    // ensure screen has changed
+    expect(find.byType(BetsListGroupView), findsOneWidget);
   });
 }
